fc6a2f2f7c65e559ca954eab2ff33e4a53f26c4d
Abinash Ghimire <119164277+Abinash7G@users.noreply.github.com>
2025-01-03 12:02:01 +0545

    created const API to connect with backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://127.0.0.1:8000/api' });
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/", // Replace with your backend base URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default API;
