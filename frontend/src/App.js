a83eb32002a73a85459fedfe7cd301617b2162bb
Abinash Ghimire <119164277+Abinash7G@users.noreply.github.com>
2025-01-03 11:41:35 +0545

    added route of login/signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            <Home />
-        </div>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
