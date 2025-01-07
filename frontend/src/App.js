f06faa93c5385be4c4c57d9da05ccf1e2f8e2525
Abinash Ghimire <119164277+Abinash7G@users.noreply.github.com>
2025-01-07 23:24:16 +0545

    fix route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; // Import `useLocation` correctly
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import CompanyRegistration from './components/companyregistration';
-import AdminDashboard from './pages/AdminDashboard.js';
-
-
-
+import AdminDashboard from './pages/AdminDashboard';
 
 const App = () => {
+  const location = useLocation(); // Correct hook name
+  const navbarRoutes = ["/", "/login", "/signup", "/companyregistration"];
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>`
+    <>
+      {navbarRoutes.includes(location.pathname) && <Navbar />}
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/companyregistration" element={<CompanyRegistration />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
+    </>
+  );
+};
+
+const AppWithRouter = () => {
+  return (
+    <Router>
+      <App />
     </Router>
   );
 };
 
-export default App;
+export default AppWithRouter;
